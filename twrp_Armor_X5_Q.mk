f084be105f7737941d4b513e28752211902992b2
spratay <spratay@linuxmail.com>
2022-07-17 18:13:06 +0300

    edit

--- twrp_Armor_X5_Q.mk
+++ twrp_Armor_X5_Q.mk
@@ -14,8 +14,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/ulefone/Armor_X5_Q/device.mk)
 
 # Inherit some common Omni stuff.
-#$(call inherit-product, vendor/omni/config/common.mk)
-#$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Armor_X5_Q
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-#add_lunch_combo twrp_Armor_X5_Q-user
-#add_lunch_combo twrp_Armor_X5_Q-userdebug
-#add_lunch_combo twrp_Armor_X5_Q-eng
-export ALLOW_MISSING_DEPENDENCIES=true
+add_lunch_combo twrp_Armor_X5_Q-user
+add_lunch_combo twrp_Armor_X5_Q-userdebug
+add_lunch_combo twrp_Armor_X5_Q-eng
+#export ALLOW_MISSING_DEPENDENCIES=true
